e32aedfc0f8f74828cb5490765331feeee1af853
Duy Thuong <62632918+duythuong2308@users.noreply.github.com>
2021-12-20 16:38:51 +0700

    add struct

--- pkg/core/schema.go
+++ pkg/core/schema.go
@@ -10,3 +10,21 @@ type District struct {
 	ProvinceId int
 	Name       string
 }
+
+type Commune struct {
+	Id 			int
+	ProvinceId	int
+	DistrictId	int
+	Name 		string
+
+}
+
+type Village struct {
+	Id 			int
+	ProvinceId	int
+	DistrictId	int
+	CommuneId	int
+	Name 		string
+	Population	int
+}
+
